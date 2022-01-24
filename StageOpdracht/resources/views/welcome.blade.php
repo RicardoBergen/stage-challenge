21edc979235843e0df40f459f488d74fab5d82e8
Ricardo <148498@student.horizoncollege.nl>
2022-01-24 19:28:14 +0100

    Project af
    De totaal score waar ik op kwam is 584

--- StageOpdracht/resources/views/welcome.blade.php
+++ StageOpdracht/resources/views/welcome.blade.php
@@ -2,14 +2,26 @@
     $alphabet = range('a', 'z');
     $werknemers = array_map('strtolower', array("Arne","Reinier","Kylian","Huub","Mick","Brenda","Ruben","Desley","Carly","Esther"));
     sort($werknemers);
-
-
+    $total = 0;
+    echo '<table>';
     foreach($werknemers as $werknemer)
     {
-        foreach( str_split($werknemer) as $letter)
-        {
-            echo $letter;
-            echo array_search($letter, $alphabet)."<br>";
-        }
+        echo '<tr>' . 
+            '<td>' . $werknemer . '</td>';
+            $first = (int)array_search(substr($werknemer, 0, 1), $alphabet) +1;
+            $subtotal = 0;
+            foreach( str_split($werknemer) as $letter)
+            {
+                $number = array_search($letter, $alphabet);
+                $subtotal += $number +1;
+            }
+            echo '<td>' . $subtotal . '</td>';
+            echo '<td> x </td>';
+            echo '<td>' . $first . '</td>';
+            echo '<td>' . $subtotal * $first . '</td>';
+        echo '</tr>';
+        $total += $subtotal;
     };
+    echo '</table>';
+    echo '<p>De totale score is: ' . $total . '</p>';
 ?>
